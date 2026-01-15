cdc934efa853741e8e09dbfb89bc0822e31eeb55
Jacob-Derington <der20003@byui.edu>
2026-01-15 12:58:04 -0700

    played with the code a bit to change sizes.

--- Week-2/ponder/dom-theme.js
+++ Week-2/ponder/dom-theme.js
@@ -8,15 +8,20 @@ function changeTheme() {
     if (current === 'ocean') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
         pageContent.style.fontFamily = "Papyrus, fantasy";
+        pageContent.style.fontSize = "1rem";
     } else if (current === 'forest') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
         pageContent.style.fontFamily = "Impact, sans-serif";
+        pageContent.style.fontSize = "3rem";
     } else if (current === 'desert') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
         pageContent.style.fontFamily = "'Big Caslon', serif";
+        pageContent.style.fontSize = "1.5rem";
     } else {
         // default
         document.body.style.backgroundImage = "none";
         pageContent.style.fontFamily = "Georgia, serif";
+        pageContent.style.fontSize = "1rem";
+        pageContent.style.content.fontSize = "1.5rem";
     }
 }
