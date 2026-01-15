f99cbddac2ffff18df9e350b680092501489c220
Jacob-Derington <der20003@byui.edu>
2026-01-15 13:01:23 -0700

    added a italic to desert and normals to every thing else.

--- Week-2/ponder/dom-theme.js
+++ Week-2/ponder/dom-theme.js
@@ -9,19 +9,23 @@ function changeTheme() {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
         pageContent.style.fontFamily = "Papyrus, fantasy";
         pageContent.style.fontSize = "1rem";
+        pageContent.style.fontStyle = "normal";
     } else if (current === 'forest') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
         pageContent.style.fontFamily = "Impact, sans-serif";
         pageContent.style.fontSize = "3rem";
+        pageContent.style.fontStyle = "normal";
     } else if (current === 'desert') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
         pageContent.style.fontFamily = "'Big Caslon', serif";
         pageContent.style.fontSize = "1.5rem";
+        pageContent.style.fontStyle = "italic";
     } else {
         // default
         document.body.style.backgroundImage = "none";
         pageContent.style.fontFamily = "Georgia, serif";
         pageContent.style.fontSize = "1rem";
-        pageContent.style.content.fontSize = "1.5rem";
+        pageContent.style.content.fontsize = "1.5rem";
+        pageContent.style.fontStyle = "normal";
     }
 }
