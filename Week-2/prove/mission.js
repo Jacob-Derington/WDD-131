7cec6cef4578c3c16f9786d4909fbad852d817d3
Jacob-Derington <der20003@byui.edu>
2026-01-17 21:24:39 -0700

    finished the dark and light mods.

--- Week-2/prove/mission.js
+++ Week-2/prove/mission.js
@@ -7,10 +7,14 @@ function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
         // code for changes to colors and logo
-        pageContent.style.color = 'gray'
-        pageContent.style.font.color = 'white'
+        document.body.style.backgroundColor = '#555555'
+        document.getElementById('content').style.color = 'white'
+        logo.src = 'byui-logo-white.png'
     } else {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = 'white'
+        document.getElementById('content').style.color = 'black'
+        logo.src = 'byui-logo-blue.webp'
     }
 }           
                
