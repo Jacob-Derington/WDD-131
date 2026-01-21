cd3e30f9113f9dbeab205ca0ec417a32b0cc00d3
Jacob-Derington <der20003@byui.edu>
2026-01-20 19:13:38 -0700

    tryed to do the js

--- Week-3/ponder/java.js
+++ Week-3/ponder/java.js
@@ -1,1 +1,6 @@
-let selectElem = ('menu-btn');
+const mb = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+mb.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
