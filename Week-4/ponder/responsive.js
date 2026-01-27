cbe26c0add4f645d9da2723b03fea6e34da5d520
Jacob-Derington <der20003@byui.edu>
2026-01-27 10:12:19 -0700

    began work on js

--- Week-4/ponder/responsive.js
+++ Week-4/ponder/responsive.js
@@ -1,17 +1,20 @@
 // const num = 7;
 
-function add(a,b) {
-    return a + b;
-}
+// function add(a,b) {
+//     return a + b;
+// }
 
-function subtract(a, b) {
-    return a - b;
-}
+// function subtract(a, b) {
+//     return a - b;
+// }
 
 // const subtract = (a, b) => {
 //     return a - b
 // };
 // also a valid function type
+
+// const subtract = (a, b) => a - b;
+// possible but dont use
 
 // ans = add(3, 6)
 // ans = subtract(ans, 2)
@@ -22,3 +25,18 @@ window.addEventListener('resize', function() {
     const width = window.innerWidth;
 });
 
+const btn = document.querySelector('.menu-btn');
+
+console.log(btn);
+
+btn.addEventListener('click', function(){
+    console.log('button clicked');
+    btn.classList.toggle('open');
+})
+
+window.addEventListener('resize', function() {
+    const width = window.innerWidth;
+    if (width >= 700) {
+        btn.classList.remove('open');
+    }
+});
