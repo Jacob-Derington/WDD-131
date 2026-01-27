afb3b48b39de410e3f3eb1218f6ff9d70df25982
Jacob-Derington <der20003@byui.edu>
2026-01-27 09:44:28 -0700

    played with functions wont use later.

---
+++ Week-4/ponder/responsive.js
@@ -0,0 +1,24 @@
+// const num = 7;
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+// const subtract = (a, b) => {
+//     return a - b
+// };
+// also a valid function type
+
+// ans = add(3, 6)
+// ans = subtract(ans, 2)
+
+// console.log(ans);
+
+window.addEventListener('resize', function() {
+    const width = window.innerWidth;
+});
+
