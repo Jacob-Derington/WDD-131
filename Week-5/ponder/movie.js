2004e616a59a31768241c6b8d32a1ce2385c5921
Jacob-Derington <der20003@byui.edu>
2026-02-03 10:08:48 -0700

    finised the js.

--- Week-5/ponder/movie.js
+++ Week-5/ponder/movie.js
@@ -1,1 +1,6 @@
 const allRatingE = document.querySelectorAll('article.movie p span');
+
+allRatingE.forEach((elem) => {
+    const rating = elem.innerHTML.length; 
+    elem.setAttribute('aria-label', `${rating} out of 5 stars`);
+});
