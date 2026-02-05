de8c41b68fa82e3bd3010f13c67fca7df3e54395
Jacob-Derington <der20003@byui.edu>
2026-02-05 09:15:01 -0700

    added blog.html and added more to movie.js.

--- Week-5/ponder/movie.js
+++ Week-5/ponder/movie.js
@@ -1,6 +1,15 @@
 const allRatingE = document.querySelectorAll('article.movie p span');
 
 allRatingE.forEach((elem) => {
-    const rating = elem.innerHTML.length; 
+    const rating = elem.innerText.length; 
     elem.setAttribute('aria-label', `${rating} out of 5 stars`);
 });
+
+// function addStars(span) {
+//     const stars = span.innerText.length;
+//     span.setAttribute('aria-label', `${stars} out of 5 stars`);
+// }
+
+// const spans = document.querySelectorAll('article.movie p span')
+
+// spans.forEach(addStars)
