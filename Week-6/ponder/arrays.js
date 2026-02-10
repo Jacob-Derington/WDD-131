00bf0a7c5b0b947d9db682a6fe94f82171a2e0a4
Jacob-Derington <der20003@byui.edu>
2026-02-10 10:24:44 -0700

    the arrays.

--- Week-6/ponder/arrays.js
+++ Week-6/ponder/arrays.js
@@ -1,8 +1,16 @@
 const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
 
 const students = [
-    {last: 'Andrus', first: 'Aaron'},
-    {last: 'Masa', first:'Manny'},
-    {last: 'Tanda', first: 'Tamanda'}
+    {last: 'Andrus', first: 'Aaron', grade: 'A'},
+    {last: 'Masa', first:'Manny', grade: 'B'},
+    {last: 'Tanda', first: 'Tamanda', grade: 'C'}
 ];
           
+ul 
+
+let html = '';
+
+words.forEach((word) => {
+    html = <li>${word}</li>
+    console.log(html)
+});
