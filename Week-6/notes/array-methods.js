ffbf6ee769abd4511915ee11286dcd0c2ff6ba70
Jacob-Derington <der20003@byui.edu>
2026-02-10 09:45:57 -0700

    am trying to play with arrays and added stuff to the code.

---
+++ Week-6/notes/array-methods.js
@@ -0,0 +1,13 @@
+const num = [1, 2, 3, 4]
+const users = []
+
+let sum = 0
+numbers.forEach((num, index) => {
+    console.log(`[$(index)]: num`)
+});
+
+const evenNumbers = numbers.filter((num) => {
+    return num % 2 === 0;
+});
+console.log(evenNumbers)
+
---
+++ Week-6/ponder/arrays.js
@@ -0,0 +1,8 @@
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const students = [
+    {last: 'Andrus', first: 'Aaron'},
+    {last: 'Masa', first:'Manny'},
+    {last: 'Tanda', first: 'Tamanda'}
+];
+          
